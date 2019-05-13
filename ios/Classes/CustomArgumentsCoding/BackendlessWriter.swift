b194e4063845ac8914d4f01fca11dc6e42b0760b
AndriiBodnar <>
2019-05-13 20:23:00 +0300

    Writer in progress

--- ios/Classes/CustomArgumentsCoding/BackendlessWriter.swift
+++ ios/Classes/CustomArgumentsCoding/BackendlessWriter.swift
@@ -24,6 +24,19 @@ class BackendlessWtiter: FlutterStandardWriter {
 //            writeGooglePlaySubscriptionStatus(value as! GooglePlaySubscriptionStatus)
 //        case is GooglePlayPurchaseStatus:
 //            writeGooglePlayPurchaseStatus(value as! GooglePlayPurchaseStatus)
+        case is BackendlessFileInfo:
+            writeFileInfo(value as! BackendlessFileInfo)
+        case is GeoCategory:
+            writeGeoCategory(value as! GeoCategory)
+        case is BackendlessGeoQuery:
+            writeGeoQuery(value as! BackendlessGeoQuery)
+        case is GeoCluster:
+            writeGeoCluster(value as! GeoCluster)
+//        case is MatchesResult:
+//            writeSearchMatchesResult(value as! SearchMatchesResult)
+        case is MessageStatus:
+            writeMessageStatus(value as! MessageStatus)
+            
         default:
             super.writeValue(value)
         }
@@ -99,13 +112,109 @@ class BackendlessWtiter: FlutterStandardWriter {
     // MARK: -
     // MARK: - Write GooglePlaySubscriptionStatus
     private func writeGooglePlaySubscriptionStatus(_ value: Any) {
+        writeValue(FlutterTypeCode.googlePlaySubscriptionStatus.rawValue)
+        
         // No realization of module in iOS SDK
     }
     
     // MARK: -
     // MARK: - Write GooglePlayPurchaseStatus
     private func writeGooglePlayPurchaseStatus(_ value: Any) {
+        writeValue(FlutterTypeCode.googlePlayPurchaseStatus.rawValue)
+        
         // No realization of module in iOS SDK
+    }
+    
+    // MARK: -
+    // MARK: - Write FileInfo
+    private func writeFileInfo(_ fileInfo: BackendlessFileInfo) {
+        writeValue(FlutterTypeCode.fileInfo.rawValue)
+        
+        fileInfo.name.map { [weak self] in self?.writeValue($0) }
+        fileInfo.createdOn.map { [weak self] in self?.writeValue($0) }
+        fileInfo.url.map { [weak self] in self?.writeValue($0) }
+        writeValue(fileInfo.size)
+    }
+    
+    // MARK: -
+    // MARK: - Write GeoCategory
+    private func writeGeoCategory(_ geoCategory: GeoCategory) {
+        writeValue(FlutterTypeCode.geoCategory.rawValue)
+        
+        geoCategory.objectId.map { [weak self] in self?.writeValue($0) }
+        geoCategory.name.map { [weak self] in self?.writeValue($0) }
+        geoCategory.size.map { [weak self] in self?.writeValue($0) }
+    }
+    
+    // MARK: -
+    // MARK: - Write GeoQuery
+    private func writeGeoQuery(_ geoQuery: BackendlessGeoQuery) {
+        writeValue(FlutterTypeCode.geoQuery.rawValue)
+        
+        geoQuery.geoPoint.map { [weak self] in self?.writeValue($0.latitude) }
+        geoQuery.geoPoint.map { [weak self] in self?.writeValue($0.longitude) }
+        geoQuery.radius.map { [weak self] in self?.writeValue($0) }
+        geoQuery.categories.map { [weak self] in self?.writeValue($0) }
+        geoQuery.metadata.map { [weak self] in self?.writeValue($0) }
+        
+        // TODO: -
+        // TODO: - How to get RelativeFindMetadata?
+        writeValue([String: String]()) // RelativeFindMetadata
+        
+        // TODO: -
+        // TODO: - How to get RelativeFindPercentThreshold?
+        writeValue(Double(0)) // RelativeFindPercentThreshold
+        
+        geoQuery.whereClause.map { [weak self] in self?.writeValue($0) }
+        
+        // TODO: -
+        // TODO: - How to get SortBy?
+        // TODO: - How to send data with Collection type in Java?
+        writeValue(0) // SortBy
+        
+        writeValue(geoQuery.degreePerPixel)
+        writeValue(geoQuery.clusterGridSize)
+        writeValue(geoQuery.pageSize)
+        writeValue(geoQuery.offset)
+        writeValue(geoQuery.getUnits()) // Need to check if Data Type is correctly readed on Flutter side
+        writeValue(geoQuery.includemetadata)
+        
+        // TODO: -
+        // TODO: - How is SearchRectangle described in iOS SDK?
+        writeValue([Double(0), Double(0)]) // SearchRectangle
+    }
+    
+    // MARK: -
+    // MARK: - Write GeoCluster
+    private func writeGeoCluster(_ geoCluster: GeoCluster) {
+        writeValue(FlutterTypeCode.geoCluster.rawValue)
+        
+        geoCluster.objectId.map { [weak self] in self?.writeValue($0) }
+        writeValue(geoCluster.latitude)
+        writeValue(geoCluster.longitude)
+        
+        // TODO: -
+        // TODO: - How to send data with Collection type in Java?
+        writeValue(geoCluster.categories)
+        geoCluster.metadata.map { [weak self] in self?.writeValue($0) }
+        
+        // TODO: -
+        // TODO: - How to get distance?
+        writeValue(Double(0)) // distance
+        writeValue(geoCluster.totalPoints)
+        geoCluster.geoQuery.map { [weak self] in self?.writeGeoQuery($0) }
+    }
+    
+    // MARK: -
+    // MARK: - Write SearchMathesResult
+//    private func writeSearchMathesResult(_ searchMathesResult: SearchMathesResult) {
+//        writeValue(FlutterTypeCode.searchMathesResult.rawValue)
+//    }
+    
+    // MARK: -
+    // MARK: -
+    private func writeMessageStatus(_ messageStatus: MessageStatus) {
+        
     }
     
     
