35ac81db5a35b0e138a86b261c8d4998961f55b4
4lfant <4lfant@gmail.com>
2020-01-08 17:21:20 +0200

    Handle error in subscribe method

--- lib/src/modules/messaging/messaging.dart
+++ lib/src/modules/messaging/messaging.dart
@@ -114,7 +114,17 @@ class BackendlessMessaging {
         "Backendless.Messaging.subscribe", <String, dynamic>{
       "channelName": channelName,
       "channelHandle": handle
-    }).then((value) => new Channel(_messagingChannel, channelName, handle));
+    }).then((value) {
+      if (value != null) {
+        return new Channel(_messagingChannel, channelName, handle);
+      } else {
+        String errorMessage = "Couldn't subscribe to the channel. ";
+        if (Platform.isAndroid) {
+          errorMessage += "Please check whether you add io.socket dependency.";
+        }
+        return Future.error(errorMessage);
+      }
+    });
   }
 
   Future<dynamic> _handleMethod(MethodCall call) async {
