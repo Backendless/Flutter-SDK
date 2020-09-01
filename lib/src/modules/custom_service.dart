81d490076fca47199defd1206874d34f2a0335a4
4lfant <4lfant@gmail.com>
2020-09-01 18:10:37 +0300

    Release 6.0.3 Fix Custom Service invocation for Web

--- lib/src/modules/custom_service.dart
+++ lib/src/modules/custom_service.dart
@@ -1,24 +1,14 @@
 part of backendless_sdk;
 
 class BackendlessCustomService {
-  static const MethodChannel _channel =
-      const MethodChannel('backendless/custom_service');
-
   factory BackendlessCustomService() => _instance;
+
   static final BackendlessCustomService _instance =
       new BackendlessCustomService._internal();
+
   BackendlessCustomService._internal();
 
-  Future<dynamic> invoke(String serviceName, String method, Map arguments) {
-    // temp workaround because Android accepts args as List
-    dynamic args =
-        (!kIsWeb && Platform.isAndroid) ? arguments.values.toList() : arguments;
-
-    return _channel
-        .invokeMethod("Backendless.CustomService.invoke", <String, dynamic>{
-      "serviceName": serviceName,
-      "method": method,
-      "arguments": args,
-    });
-  }
+  Future<dynamic> invoke(
+          String serviceName, String method, dynamic arguments) =>
+      Invoker.invoke("services/$serviceName/$method", arguments);
 }
