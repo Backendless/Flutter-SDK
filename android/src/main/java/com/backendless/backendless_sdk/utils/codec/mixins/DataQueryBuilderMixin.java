2fec1483afb74efe5741cec332f0f502eeaf6080
4lfant <4lfant@gmail.com>
2020-10-01 16:04:37 +0300

    21138 Fix relationPageSize property

--- android/src/main/java/com/backendless/backendless_sdk/utils/codec/mixins/DataQueryBuilderMixin.java
+++ android/src/main/java/com/backendless/backendless_sdk/utils/codec/mixins/DataQueryBuilderMixin.java
@@ -1,15 +1,11 @@
 package com.backendless.backendless_sdk.utils.codec.mixins;
 
 import com.backendless.persistence.DataQueryBuilder;
-import com.fasterxml.jackson.annotation.JsonGetter;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.ArrayList;
 import java.util.List;
 
-
-@JsonIgnoreProperties({"relationsPageSize"})
 public abstract class DataQueryBuilderMixin {
 
     @JsonProperty("properties")
